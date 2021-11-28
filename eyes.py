56e2582a1474dd032ad27990496aee19e2f9ea04
piemanabc <67368507+piemanabc@users.noreply.github.com>
2021-11-28 22:00:24 +1300

    added blink

--- eyes.py
+++ eyes.py
@@ -3,7 +3,8 @@ from time import sleep
 
 
 # Eye LEDs
-eye_led = gpiozero.LED(6)
+l_eye_led = gpiozero.PWMLED()  # Needs Pin
+r_eye_led = gpiozero.PWMLED()  # Needs Pim
 
 # Eye 0 (left)
 l_eye = gpiozero.MotionSensor(17)
@@ -18,14 +19,25 @@ while True:
     if l_eye.motion_detected:
         print("left motion!")
         servo.value = -1
-        sleep(2)
+        l_eye_led.blink(on_time=2, fade_in_time=1, fade_out_time=1, n=1) and r_eye_led.blink(on_time=2, fade_in_time=1,
+                                                                                             fade_out_time=1, n=1)
+        sleep(1)
 
-    elif l_eye.motion_detected and r_eye.motion_detected or not l_eye.motion_detected and not r_eye.motion_detected:
+    elif l_eye.motion_detected and r_eye.motion_detected:
         print("middle motion or I cant see you!")
         servo.value = 0
-        sleep(2)
+        l_eye_led.blink(on_time=2, fade_in_time=1, fade_out_time=1, n=1) and r_eye_led.blink(on_time=2, fade_in_time=1,
+                                                                                             fade_out_time=1, n=1)
+        sleep(1)
 
     elif r_eye.motion_detected:
         servo.value = 1
         print("right motion!")
-        sleep(2)
+        l_eye_led.blink(on_time=2, fade_in_time=1, fade_out_time=1, n=1) and r_eye_led.blink(on_time=2, fade_in_time=1,
+                                                                                             fade_out_time=1, n=1)
+        sleep(1)
+
+    elif not l_eye.motion_detected and not r_eye.motion_detected:
+        servo.value = 0
+        print("where are you?")
+        sleep(1)
