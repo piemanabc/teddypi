e6b08ae6324a7970d4599442d4d4332685baf83c
piemanabc <67368507+piemanabc@users.noreply.github.com>
2021-11-28 15:19:45 +1300

    sanity check

--- eyes.py
+++ eyes.py
@@ -9,20 +9,19 @@ from time import sleep
 eye_led = gpiozero.LED(6)
 
 # Eye 0 (left)
-l_pin_in = 3
-l_pin_out = 7
+l_eye = gpiozero.MotionSensor(3)
 
 # Eye 1 (right)
-r_pin_in = 2
-R_pin_out = 8
+r_eye = gpiozero.MotionSensor(2)
 
 # Servo
 servo = gpiozero.Servo(4)
 
 while True:
-    servo.min()
-    sleep(1)
-    servo.mid()
-    sleep(1)
-    servo.max()
-    sleep(1)
+    if l_eye.motion_detected():
+        servo.min()
+        sleep(1)
+        servo.mid()
+        sleep(1)
+        servo.max()
+        sleep(1)
