427b0dabace186820c46332b2fbcb9430fd69e96
piemanabc <67368507+piemanabc@users.noreply.github.com>
2021-11-28 15:37:41 +1300

    vision script v1

--- eyes.py
+++ eyes.py
@@ -12,16 +12,23 @@ eye_led = gpiozero.LED(6)
 l_eye = gpiozero.MotionSensor(17)
 
 # Eye 1 (right)
-# r_eye = gpiozero.MotionSensor(2)
+r_eye = gpiozero.MotionSensor(27)
 
 # Servo
 servo = gpiozero.Servo(4)
 
 while True:
     if l_eye.motion_detected():
-        servo.min()
-        sleep(1)
-        servo.mid()
-        sleep(1)
-        servo.max()
-        sleep(1)
+        print("left motion!")
+        servo.value = -1
+        sleep(2)
+
+    elif l_eye.motion_detected and r_eye.motion_detected or not l_eye.motion_detected and not r_eye.motion_detected:
+        print("middle motion or I cant see you!")
+        servo.value = 0
+        sleep(2)
+
+    elif r_eye.motion_detected:
+        servo.value = 1
+        print("right motion!")
+        sleep(2)
