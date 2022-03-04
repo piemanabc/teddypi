aa8ccc7b7309f9faf2c76a70ff7614c3cd8ee793
piemanabc <67368507+piemanabc@users.noreply.github.com>
2022-03-04 18:52:56 +1300

    changed order of operations

--- eyes.py
+++ eyes.py
@@ -18,20 +18,20 @@ servo = gpiozero.Servo(4)
 # loop forever
 
 while True:
-    # if Motion seen on the left of the device (from teddy pov)
+    # if Motion seen on the left and right of the device (from teddy pov)
 
-    if l_eye.motion_detected:
-        print("left motion!")
-        servo.value = -1
+    if l_eye.motion_detected and r_eye.motion_detected:
+        print("middle motion or I cant see you!")
+        servo.value = 0
         l_eye_led.blink(on_time=2, fade_in_time=1, fade_out_time=1, n=1) and r_eye_led.blink(on_time=2, fade_in_time=1,
                                                                                              fade_out_time=1, n=1)
         sleep(1)
 
-    # if Motion seen on the left and right of the device (from teddy pov)
+    # if Motion seen on the left of the device (from teddy pov)
 
-    elif l_eye.motion_detected and r_eye.motion_detected:
-        print("middle motion or I cant see you!")
-        servo.value = 0
+    elif l_eye.motion_detected:
+        print("left motion!")
+        servo.value = -1
         l_eye_led.blink(on_time=2, fade_in_time=1, fade_out_time=1, n=1) and r_eye_led.blink(on_time=2, fade_in_time=1,
                                                                                              fade_out_time=1, n=1)
         sleep(1)
